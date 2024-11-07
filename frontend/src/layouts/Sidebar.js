48fdc7332470ae4bc4dc2335d55a2b29611eba3e
10-6\kkw <jscode77@gmail.com>
2024-11-07 10:06:33 +0900

    main 메뉴 1차

--- frontend/src/layouts/Sidebar.js
+++ frontend/src/layouts/Sidebar.js
@@ -1,66 +1,29 @@
-import { Button, Nav, NavItem } from "reactstrap";
-import { Link, useLocation } from "react-router-dom";
+import {  Button, DropdownItem, Nav, NavItem } from "reactstrap";
+import {  Link, useLocation } from "react-router-dom";
 import user1 from "../assets/images/users/user4.jpg";
 import probg from "../assets/images/bg/download.jpg";
+import PageInfo from "./PageInfo";
 
-const navigation = [
-  {
-    title: "Dashboard",
-    href: "/starter",
-    icon: "bi bi-speedometer2",
-  },
-  {
-    title: "Alert",
-    href: "/alerts",
-    icon: "bi bi-bell",
-  },
-  {
-    title: "Badges",
-    href: "/badges",
-    icon: "bi bi-patch-check",
-  },
-  {
-    title: "Buttons",
-    href: "/buttons",
-    icon: "bi bi-hdd-stack",
-  },
-  {
-    title: "Cards",
-    href: "/cards",
-    icon: "bi bi-card-text",
-  },
-  {
-    title: "Grid",
-    href: "/grid",
-    icon: "bi bi-columns",
-  },
-  {
-    title: "Table",
-    href: "/table",
-    icon: "bi bi-layout-split",
-  },
-  {
-    title: "Forms",
-    href: "/forms",
-    icon: "bi bi-textarea-resize",
-  },
-  {
-    title: "Breadcrumbs",
-    href: "/breadcrumbs",
-    icon: "bi bi-link",
-  },
-  {
-    title: "About",
-    href: "/about",
-    icon: "bi bi-people",
-  },
-];
+
+/*메뉴 내용->PagiInfo */
+
+
+
 
 const Sidebar = () => {
+  
+  
   const showMobilemenu = () => {
     document.getElementById("sidebarArea").classList.toggle("showSidebar");
   };
   let location = useLocation();
+
+  /*custom method*/
+  const {menu1, menu2, menu3, menu4} = PageInfo();
+  console.log(menu1);
+  console.log(menu2);
+  console.log(menu3);
+  console.log(menu4);
 
   return (
     <div>
@@ -83,21 +46,69 @@ const Sidebar = () => {
       </div>
       <div className="p-3 mt-2">
         <Nav vertical className="sidebarNav">
-          {navigation.map((navi, index) => (
-            <NavItem key={index} className="sidenav-bg">
-              <Link
-                to={navi.href}
-                className={
-                  location.pathname === navi.href
-                    ? "active nav-link py-3"
-                    : "nav-link text-secondary py-3"
-                }
-              >
-                <i className={navi.icon}></i>
-                <span className="ms-3 d-inline-block">{navi.title}</span>
-              </Link>
-            </NavItem>
-          ))}
+        <NavItem className="sidenav-bg">
+          <Link
+            to={menu1.topMenu.href}
+            className={
+              location.pathname === menu1.topMenu.href
+                ? "active nav-link py-3"
+                : "nav-link text-secondary py-3"}>
+            <i className={menu1.topMenu.icon}></i>
+            <span className="ms-3 d-inline-block">{menu1.topMenu.title}</span>
+          </Link>
+        </NavItem>
+        <NavItem className="sidenav-bg">
+          <Link
+            to={menu2.topMenu.href}
+            className={
+              location.pathname === menu2.topMenu.href
+                ? "active nav-link py-3"
+                : "nav-link text-secondary py-3"}>
+            <i className={menu2.topMenu.icon}></i>
+            <span className="ms-3 d-inline-block">{menu2.topMenu.title}</span>
+          </Link>
+        </NavItem>
+        {menu2.subMenus.map(sub=>(
+          <ul className="offset-1">
+            <Link to={sub.href}><li>{sub.title}</li></Link>
+          </ul>
+        ))}
+        <NavItem className="sidenav-bg">
+          <Link
+            to={menu3.topMenu.href}
+            className={
+              location.pathname === menu3.topMenu.href
+                ? "active nav-link py-3"
+                : "nav-link text-secondary py-3"}>
+            <i className={menu3.topMenu.icon}></i>
+            <span className="ms-3 d-inline-block">{menu3.topMenu.title}</span>
+          </Link>
+        </NavItem>
+        {menu3.subMenus.map(sub=>(
+          <ul className="offset-1">
+            <Link to={sub.href}><li>{sub.title}</li></Link>
+          </ul>
+        ))}
+        <NavItem className="sidenav-bg">
+          <Link
+            to={menu4.topMenu.href}
+            className={
+              location.pathname === menu4.topMenu.href
+                ? "active nav-link py-3"
+                : "nav-link text-secondary py-3"}>
+            <i className={menu4.topMenu.icon}></i>
+            <span className="ms-3 d-inline-block">{menu4.topMenu.title}</span>
+          </Link>
+        </NavItem>
+        {menu4.subMenus.map(sub=>(
+          <ul className="offset-1">
+            <Link to={sub.href}><li>{sub.title}</li></Link>
+          </ul>
+        ))}
+
+
+
+
           <Button
             color="danger"
             tag="a"
@@ -105,7 +116,7 @@ const Sidebar = () => {
             className="mt-3"
             href="https://wrappixel.com/templates/materialpro-react-admin/?ref=33"
           >
-            Upgrade To Pro
+            결제
           </Button>
         </Nav>
       </div>
